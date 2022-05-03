cd7ad3517f77ec155b57c4d56c0ae0d895c698c3
PipsaKorkiakoski <pipsa.korkiakoski@gmail.com>
2022-05-03 14:49:32 +0300

    rm pg

--- Dockerfile
+++ Dockerfile
@@ -4,6 +4,8 @@ WORKDIR /app
 
 COPY . .
 
-RUN npm install --production
+RUN npm install
 
 EXPOSE 8080
+
+CMD [ "node", "index.js" ]
--- index.js
+++ index.js
@@ -1,57 +1,28 @@
 const express = require('express');
-const { Sequelize, DataTypes } = require('sequelize')
-const sequelize = new Sequelize(process.env.DATABASE_URL, {
-    dialect: 'postgres',
-    dialectOptions: {
-        ssl: {
-            require: true,
-            rejectUnauthorized: false
-        }
-    }
-})
 
-const SensorData = sequelize.define('sensor-data', {
-    serial: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    name: {
-        type: DataTypes.STRING,
-        allowNull: false
-    },
-    temperature: {
-        type: DataTypes.FLOAT,
-        allowNull: false
-    }
-})
+
 const app = express();
 app.use(express.json());
 
 const data = [];
-
+app.get('/', (req, res) => {
+    res.status(200).send('Welcome');
+    return;
+})
 app.get('/hello', (req, res) => {
     res.status(200).send('hello');
     return;
 })
 app.get('/data', async (req, res) => {
-    const allData = await SensorData.findAll()
-    res.status(200).send(allData);
+    res.status(200).send(data);
     return;
 })
-app.post('/data', async (req, res) => {
+app.post('/data', (req, res) => {
     let dataBody = req.body;
-    const sensorData = await SensorData.create(dataBody)
-    res.status(201).send(dataBody);
+    data.push(dataBody)
+    res.status(201).send(data);
     return;
 })
 app.listen({ port: 8080 }, () => {
-    try {
-        sequelize.authenticate();
-        console.log('Connected to database')
-        sequelize.sync({ alter: true })
-        console.log("Synced")
-    } catch (error) {
-        console.log('Connection error..', error)
-    }
     console.log('Server is running...')
 })
